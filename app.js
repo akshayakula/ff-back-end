b584f09148d40f2c4721f6762e5a2e87deda1826
akshayakula <akshayakula@vt.edu>
2022-03-26 16:28:35 -0400

    base code

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
